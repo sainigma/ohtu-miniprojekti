285b54d01e24217856a7ffc40fcfaba0a1468fbc
sainigma <sainigma@gmail.com>
2021-11-23 12:09:49 +0200

    configured poetry, added dev dependencies, added a dummy database to tests

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
 ## Ohtu-miniprojekti
 
-<<<<<<< HEAD
 Veikko is here
 Joonas is here
-=======
-Veikko is here 2.0
->>>>>>> 26e73ece1b6f2def63ec7ac22a864218d244d15f
---
+++ src/tests/dummy.js
@@ -0,0 +1,57 @@
+{
+  [
+    {
+      "id:":2344,
+      "title":"Minimientry",
+      "tags":[]
+    },
+    {
+      "id":0,
+      "title":"Esimerkkientry",
+      "tags":[
+        {
+          "type":"Tagin tyyppi",
+          "content":"Tagin sisältö"
+        },
+        {
+          "type":"Tagin tyyppi",
+          "content":"Tagit palautetaan listoina"
+        },
+        {
+          "type":"Tagin tyyppi",
+          "content":"Tagit tallennetaan erilliseen tauluun, tässä valmiiksi aggregoituna"
+        }
+      ]
+    },
+    {
+      "id":1,
+      "title":"Clean Code: A Handbook of Agile Software Craftsmanship",
+      "tags":[
+        {
+          "type":"Kirjoittaja",
+          "content":"Robert Martin"
+        },
+        {
+          "type":"tyyppi",
+          "content":"Kirja"
+        },
+        {
+          "type":"ISBN",
+          "content":"978-0132350884"
+        },
+        {
+          "type":"Tagit",
+          "content":"Ohjelmointi"
+        },
+        {
+          "type":"Tagit",
+          "content":"design patterns"
+        },
+        {
+          "type":"Related courses",
+          "content": "TKT20006 Ohjelmistotuotanto"
+        }
+      ]
+    }
+  ]
+}

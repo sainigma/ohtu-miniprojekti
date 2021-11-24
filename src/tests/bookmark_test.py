f6545961c503a8926bd4a1fe3d14a46e7a6d8d65
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-11-24 11:37:16 +0200

    Uncommented bookmark and its tests

--- src/tests/bookmark_test.py
+++ src/tests/bookmark_test.py
@@ -1,12 +1,12 @@
-# import unittest
-# from bookmark import Bookmark
+import unittest
+from bookmark import Bookmark
 
-# class TestBookmark(unittest.TestCase):
-#     def setUp(self):
-#         pass
+class TestBookmark(unittest.TestCase):
+    def setUp(self):
+        pass
 
-    # def test_constructor_creates_object_correctly(self):
-    #     bookmark = Bookmark("123456", "Kirja")
+    def test_constructor_creates_object_correctly(self):
+        bookmark = Bookmark("123456", "Kirja")
 
-    #     self.assertEqual(bookmark.id, "123456")
-    #     self.assertEqual(bookmark.title, "Kirja")
+        self.assertEqual(bookmark.id, "123456")
+        self.assertEqual(bookmark.title, "Kirja")

945efa45fa53e6cc09f46e42a92c0208144a5740
Joonas Maisala <joonas-maisala@hotmail.com>
2021-11-26 13:28:48 +0200

    Input trimming added. Log.htlm added

--- src/app.py
+++ src/app.py
@@ -20,6 +20,7 @@ class App:
         return self.ui.read("Give command: ")
 
     def parse_input(self, input):
+        input = input.strip()
         if input == "q":
             self.quit()
         if input == "add":

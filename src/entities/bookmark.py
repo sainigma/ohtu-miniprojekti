06058617a05f21f2cb4b0bcc436dd59abad19f53
Oskar Sjölund <oskar.sjolund93@gmail.com>
2021-11-26 11:32:01 +0200

    bookmark add_tag, find_tag and get_bookmark added

--- src/entities/bookmark.py
+++ src/entities/bookmark.py
@@ -1,7 +1,25 @@
-from dataclasses import dataclass
-
-@dataclass
 class Bookmark:
-    id:str
-    title:str
+    def __init__(self, id, name):
+        self.id = id
+        self.title = name
+        self.tags = set()
     
+    def add_tag(self, tag):
+        #add tag if not already found
+        if tag.lower() not in self.tags:
+            self.tags.add(tag.lower())
+    
+    def find_tag(self, tag):
+        #search for tag, return true if found, false if not
+        if tag.lower() in self.tags:
+            return True
+        return False
+
+    def get_bookmark(self):
+        # create and return a dictionary with bookmark object fields
+        bookmark = {
+            "id":   self.id,
+            "name": self.title,
+            "tags": self.tags
+        }
+        return bookmark
--- src/tests/bookmark_test.py
+++ src/tests/bookmark_test.py
@@ -3,10 +3,36 @@ from entities.bookmark import Bookmark
 
 class TestBookmark(unittest.TestCase):
     def setUp(self):
-        pass
+        self.bookmark = Bookmark("2222", "Kirja")
 
-    def test_constructor_creates_object_correctly(self):
+    def test_constructor_creates_bookmark(self):
         bookmark = Bookmark("123456", "Kirja")
 
         self.assertEqual(bookmark.id, "123456")
         self.assertEqual(bookmark.title, "Kirja")
+    
+    def test_add_tag_functions_properly(self):
+        self.bookmark.add_tag("tira")
+
+        self.assertIn("tira", self.bookmark.tags)
+    
+    def test_no_duplicate_tags_allowed(self):
+        self.bookmark.add_tag("Tira")
+        self.bookmark.add_tag("tira")
+        self.assertSetEqual(self.bookmark.tags, {"tira"})
+
+    def test_get_bookmark_returns_correct_dict(self):
+        self.bookmark.add_tag("tira")
+        self.bookmark.add_tag("tärkeä")
+
+        self.assertDictEqual(self.bookmark.get_bookmark(),
+        {"id": "2222",
+        "name": "Kirja",
+        "tags": {"tira", "tärkeä"}})
+    
+    def test_find_tag_returns_true_if_found(self):
+        self.bookmark.add_tag("tira")
+        self.bookmark.add_tag("tärkeä")
+        self.assertTrue(self.bookmark.find_tag("tira"))
+        self.assertTrue(self.bookmark.find_tag("tärkeä"))
+        self.assertFalse(self.bookmark.find_tag("ei löydy"))

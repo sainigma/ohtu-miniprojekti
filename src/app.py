b0a71a65cabbeaee2128ab0600c8b8f7506e0575
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-11-26 10:47:23 +0200

    App commands split into methods, added a testcase

--- src/app.py
+++ src/app.py
@@ -5,25 +5,54 @@ class App:
         self.ui = ui
         
     def run(self):
-        self.ui.write("Welcome to Bookmarker!")
+        self.welcome()
         while True:
-            command = self.ui.read("Give command: ")
-            if command == "q":
-                self.ui.write("See you again!")
-                break
-            if command == "add":
-                self.ui.write("Add-command is not yet implemented")
-            elif command == "show":
-                self.ui.write("Show-command is not yet implemented")
-            elif command == "edit":
-                self.ui.write("Edit-command is not yet implemented")
-            else:
-                self.ui.write("""
-                Acceptable commands:
-                'q' - quit,
-                'add' - add a new bookmark,
-                'show' - show given amount of bookmarks,
-                'edit' - edit a bookmark
-                """)
+            
+            command = self.read_input()
+            self.parse_input(command)
+
+
+    def read_input(self):
+        return self.ui.read("Give command: ")
+    
+
+    def parse_input(self, input):
+        if input == "q":
+            self.quit()
+        if input == "add":
+            self.ui.write("Add-command is not yet implemented")
+        elif input == "show":
+            self.ui.write("Show-command is not yet implemented")
+        elif input == "edit":
+            self.ui.write("Edit-command is not yet implemented")
+        else:
+            self.usage()
+
+    def welcome(self):
+        self.ui.write("Welcome to Bookmarker!")
+
+    def quit(self):
+        self.ui.write("See you again!")
+    
+    def add(self):
+        self.ui.write("Add-command is not yet implemented")
+    
+    def show(self):
+        self.ui.write("Show-command is not yet implemented")
+    
+    def edit(self):
+        self.ui.write("Edit-command is not yet implemented")
+
+    def usage(self):
+        self.ui.write("""
+            Acceptable commands:
+            'q' - quit,
+            'add' - add a new bookmark,
+            'show' - show given amount of bookmarks,
+            'edit' - edit a bookmark
+        """)
+
+
+
 
 app = App(ui=ConsoleIO())
---
+++ src/tests/app_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from app import App
+from ui.stub_io import StubIO
+
+class AppTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.io = StubIO()
+        self.app = App(self.io)
+    
+    def test_welcome_message(self):
+        
+        self.app.welcome()
+        self.assertEqual(self.io.outputs[0], "Welcome to Bookmarker!")
+

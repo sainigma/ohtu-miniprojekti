3fd380a676e0450ff78b252657c0ee92827986b6
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-11-30 11:53:26 +0200

    Move robot tests to their own module 2

---
+++ src/tests/robot/AppLibrary.py
@@ -0,0 +1,56 @@
+from ui.stub_io import StubIO
+from ui.app_ui import AppUi
+
+
+class AppLibrary:
+    '''
+        References from course material
+    '''
+
+    def __init__(self):
+        self._io = StubIO()
+        self._app = AppUi(self._io)
+
+    def input(self, value):
+        self._io.set_input(value)
+    
+    def read_command(self):
+        self._app.parse_command(self._io.read(prompt=""))
+    
+    def read_title(self):
+        self._app.add_bookmark(self._io.read(prompt=""))
+    
+    def read_search_term(self):
+        self._app.search_by_title(self._io.read(prompt=""))
+
+    def output_should_contain(self, value):
+        output = self._io.output
+
+        if not value in output:
+            raise AssertionError(
+                f"Output \"{value}\" is not in {str(output)}"
+            )
+        
+    def output_should_be(self, value):
+        if not value == self._io.output:
+            raise AssertionError(
+                f"Output \"{value}\" is not equal to {str(self._io.output)}"
+            )
+    
+    def prompt_should_contain(self, value):
+        prompt = self._io.prompt
+
+        if not value in prompt:
+            raise AssertionError(
+                f"Output \"{value}\" is not in {str(prompt)}"
+            )
+
+    
+    def welcome(self):
+        self._app.welcome()
+
+    def reset(self):
+        self._app.repository.clear()
+    
+    def add_bookmark(self, title):
+        self._app.add_bookmark(title)
---
+++ src/tests/robot/resource.robot
@@ -0,0 +1,29 @@
+*** Settings ***
+Library  ../../app.py
+Library  AppLibrary.py
+
+*** Variables ***
+${EMPTY}   ""
+
+*** Keywords ***
+Input Quit Command
+    Input  q
+    Read Command
+
+Input Add Command
+    Input  add
+    Read Command
+
+Input Show Command
+    Input  show
+    Read Command
+
+Input Find Command
+    Input  search
+    Read Command
+
+Input Empty Command
+    Input  ${EMPTY}
+    Read Command
+
+

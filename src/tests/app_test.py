bfe263e75a5ae54fed39c496cfd86ce3f02f8cd2
Akihiro Tanaka <akihiro.fin@gmail.com>
2021-12-06 21:41:42 +0200

    Added a test for app.py

--- src/tests/app_test.py
+++ src/tests/app_test.py
@@ -1,13 +1,17 @@
 import unittest
 
 from app import App
-from ui.stub_io import StubIO
+from unittest.mock import Mock
 
 
 class AppTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.io = StubIO()
+        self.io = Mock()
         self.app = App(self.io)
 
-    def test_welcome_message(self):
-        pass
+    def test_close_the_app_if_command_q(self):
+        self.io.read_input.return_value = "q"
+        with self.assertRaises(SystemExit) as cm:
+            self.app.run()
+        
+        self.assertEqual(cm.exception.code, 0)

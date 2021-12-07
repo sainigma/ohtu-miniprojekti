6121579113ffd2687e986325eec27fb64c427ae9
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2021-12-07 12:10:48 +0200

    Use custom Exception class for input errors

--- src/ui/app_ui.py
+++ src/ui/app_ui.py
@@ -1,3 +1,4 @@
+from commands.commands import InvalidInputException
 from ui.console_io import console_io as default_console_io
 from services.bookmarks_service import bookmarks_service as default_bookmarks_service
 from commands.command_factory import CommandFactory
@@ -23,7 +24,10 @@ class AppUi:
         self.command = self.command_factory.get_command(arguments[0])
     
     def execute_command(self):
-        self.command.execute(self.argv)
+        try:
+            self.command.execute(self.argv)
+        except InvalidInputException as error:
+            self.io.write(str(error))
 
     def welcome(self):
         self.io.write("\nWelcome to Bookmarker!\nType 'h' for help\n")
